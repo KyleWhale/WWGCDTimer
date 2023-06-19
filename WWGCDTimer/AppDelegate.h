b7545409b930ab6f171c4747f4a8c73ace8653f0
kylej <kylej@bluewhaletech.pro>
2023-06-19 11:10:35 +0800

    init

---
+++ WWGCDTimer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WWGCDTimer
+//
+//  Created by 李雪健 on 2023/6/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ WWGCDTimer/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  WWGCDTimer
+//
+//  Created by 李雪健 on 2023/6/19.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
